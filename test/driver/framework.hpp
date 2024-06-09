3c1ee3a14bd89625052b07c4e55b3b21d2b4f37d
donnaskiez <lachiehodges7785@gmail.com>
2024-06-09 22:09:18 +1000

    BIG CLEANUP

--- test/driver/framework.hpp
+++
@@ -1,9 +0,0 @@
-#include "common.hpp"
-
-namespace framework
-{
-	class state
-	{
-
-	};
-}
--- test/driver/patch.cpp
+++
@@ -1,7 +0,0 @@
-#include "patch.hpp"
-
-namespace framework {
-patch::patch(char *image_name) {}
-
-patch::~patch() {}
-} // namespace framework
